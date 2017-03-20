f9a0bff8d81f6dfcd3178827539b2b31a152d209
Konrad Straszewski <koonradstraszewski@gmail.com>
2017-03-20 13:22:08 +0100

    wykres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
@@ -6,6 +6,7 @@ import { Page2 } from '../pages/page2/page2';
 import { LoginPage } from "../pages/login/login";
 import {HomePage} from "../pages/home/home";
 import {AuthService} from "../providers/auth-service";
+import {nvD3} from "ng2-nvd3";
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {AuthService} from "../providers/auth-service";
     Page1,
     Page2,
     LoginPage,
-    HomePage
+    HomePage,
+    nvD3
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -24,8 +26,10 @@ import {AuthService} from "../providers/auth-service";
     Page1,
     Page2,
     LoginPage,
-    HomePage
+    HomePage,
+
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, AuthService]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, AuthService],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule {}
---
+++ src/components/chart/chart.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {nvD3} from 'ng2-nvd3'
+declare let d3: any;
+/*
+ Generated class for the Chart component.
+
+ See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+ for more info on Angular 2 Components.
+ */
+@Component({
+  selector: 'my-chart',
+  templateUrl: 'chart.html',
+})
+export class ChartComponent{
+
+  options;
+  data;
+
+
+
+  constructor() {  }
+
+}
